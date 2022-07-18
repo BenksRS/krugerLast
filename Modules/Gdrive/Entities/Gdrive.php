ec2b9af880ad50785cd1d199849763f4df194ace
Felipe <felipepinheirosilva@gmail.com>
2022-07-18 11:18:41 -0400

    d

--- Modules/Gdrive/Entities/Gdrive.php
+++ Modules/Gdrive/Entities/Gdrive.php
@@ -5,7 +5,7 @@ namespace Modules\Gdrive\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class gdrive extends Model
+class Gdrive extends Model
 {
     use HasFactory;
     protected $table    = 'gdrive';
