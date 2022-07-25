9697a0b68f00240a0bee634536bef5c95351f53b
Michel Vieira <michelvieira@outlook.com>
2022-07-25 14:34:27 -0300

    Update WorkerRepository.php

--- Modules/Integration/Repositories/WorkerRepository.php
+++ Modules/Integration/Repositories/WorkerRepository.php
@@ -13,7 +13,7 @@ class WorkerRepository extends Model {
 
     public function user ()
     {
-        return $this->belongsTo(User::class);
+        return $this->hasOne(User::class);
     }
 
     public function resources ()
