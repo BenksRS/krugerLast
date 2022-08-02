a0338a2a756e01eeb40dbdde3f1b91903ee15674
Felipe <felipepinheirosilva@gmail.com>
2022-08-02 14:09:02 -0400

    prospect

---
+++ Modules/Referrals/Http/Livewire/New/NewProspect.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Referrals\Http\Livewire\New;
+
+use Livewire\Component;
+
+class NewProspect extends Component
+{
+    public function render()
+    {
+        return view('referrals::livewire.new.new-prospect');
+    }
+}
