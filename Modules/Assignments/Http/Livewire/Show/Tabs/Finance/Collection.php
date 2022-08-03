ef5ace61fb2122354d7ad8ec17e758f83040ffda
Felipe <felipepinheirosilva@gmail.com>
2022-08-03 11:55:38 -0400

    lien_info

--- Modules/Assignments/Http/Livewire/Show/Tabs/Finance/Collection.php
+++ Modules/Assignments/Http/Livewire/Show/Tabs/Finance/Collection.php
@@ -13,21 +13,48 @@ class Collection extends Component
     ];
     public $assignment;
     public $showFollowUp=true;
+    public $showLien=true;
     public $follow_up_date;
+    public $lien_date_m;
+    public $lien_info;
 
     public function mount(AssignmentFinanceRepository $assignment)
     {
         $this->assignment = $assignment;
         $this->follow_up_date = $this->assignment->follow_up;
+        $this->lien_date_m = $this->assignment->lien_date;
+        $this->lien_info = $this->assignment->lien_info;
     }
     public function updateFollowup($formData){
 
         $this->assignment->update($formData);
         $this->assignment = AssignmentFinanceRepository::find($this->assignment->id);
+
         $this->toggleFollowup();
     }
+    public function setCollectionStatus($id){
+        $update['status_collection_id']=$id;
+        $this->assignment->update($update);
+        $this->assignment = AssignmentFinanceRepository::find($this->assignment->id);
+    }
 
+    public function updateLien($formData){
 
+        $formData['status_id']=9;
+        $formData['status_collection_id']=9;
+        $this->assignment->update($formData);
+        $this->assignment = AssignmentFinanceRepository::find($this->assignment->id);
+        $this->toggleLien();
+
+        $this->emit('updateScheduling');
+    }
+
+    public function toggleLien(){
+        $this->assignment = AssignmentFinanceRepository::find($this->assignment->id);
+        $this->showLien=!$this->showLien;
+
+        $this->assignment = AssignmentFinanceRepository::find($this->assignment->id);
+    }
     public function toggleFollowup(){
 
         $this->assignment = AssignmentFinanceRepository::find($this->assignment->id);
--- Modules/Assignments/Resources/views/livewire/show/tabs/finance/collection.blade.php
+++ Modules/Assignments/Resources/views/livewire/show/tabs/finance/collection.blade.php
@@ -39,18 +39,18 @@
             <div class="col-lg-5">
                 <h7 class="mb-1 text-end">Collection Status:
                     <span class="badge {{$assignment->status_collection->class}}" style="font-weight:600;">{{$assignment->status_collection->name}}</span>
-{{--                    <div class="btn-group float-end">--}}
-{{--                        <button type="button" class="btn btn-sm btn-secondary dropdown-toggle " data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">--}}
-{{--                            <i class="bx bx-dots-horizontal align-middle"></i>--}}
-{{--                        </button>--}}
-{{--                        <ul class="dropdown-menu dropdown-menu-lg-end">--}}
-{{--                            <li><button class="dropdown-item" wire:click="setPreStatus(9)" type="button">LIEN PLACED</button></li>--}}
-{{--                            <li><button class="dropdown-item" wire:click="setPreStatus(19)" type="button">PENDING SOLUTION</button></li>--}}
-{{--                            <li><button class="dropdown-item" wire:click="changeStatus(21)"  type="button">CALLED</button></li>--}}
-{{--                            <li><button class="dropdown-item" wire:click="changeStatus(22)" type="button">PAYMENT AGREEMENT</button></li>--}}
-{{--                            <li><button class="dropdown-item" wire:click="changeStatus(11)"  type="button">LEFT MESSAGE</button></li>--}}
-{{--                        </ul>--}}
-{{--                    </div>--}}
+                    <div class="btn-group float-end">
+                        <button type="button" class="btn btn-sm btn-secondary dropdown-toggle " data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                            <i class="bx bx-dots-horizontal align-middle"></i>
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-lg-end">
+                            <li><button class="dropdown-item" wire:click="setCollectionStatus(9)" type="button">LIEN PLACED</button></li>
+                            <li><button class="dropdown-item" wire:click="setCollectionStatus(19)" type="button">PENDING SOLUTION</button></li>
+                            <li><button class="dropdown-item" wire:click="setCollectionStatus(21)"  type="button">CALLED</button></li>
+                            <li><button class="dropdown-item" wire:click="setCollectionStatus(22)" type="button">PAYMENT AGREEMENT</button></li>
+                            <li><button class="dropdown-item" wire:click="setCollectionStatus(11)"  type="button">LEFT MESSAGE</button></li>
+                        </ul>
+                    </div>
                 </h7>
 {{--                <button type="button" wire:click="showAdd" class="btn btn-sm btn-primary  waves-effect waves-light  me-2 float-end"> <i class="bx bx-flag font-size-16 align-middle "></i> </button>--}}
             </div>
@@ -81,10 +81,40 @@
                 </div>
                 <div class="col-lg-7">
                     <div class="text-muted mt-3">
-                        <h6>Lien Date :</h6>
+                        @if($showLien)
+                        <h6>Lien Date : <small class="text-muted">{{$assignment->lien_date_view}} </small>
+                            <button type="button" class="btn btn-primary btn-sm float-end" wire:click="toggleLien"><i class="fas fa-edit"></i> Edit</button></h6>
                         <p></p>
-                        <h6>Lien Info:</h6>
-                        <p></p>
+                        <h6>Lien Info:
+                            <small class="text-muted"> {{$assignment->lien_info}}</small>
+                        </h6>
+                        @else
+                            <h6>Lien Date:</h6>
+                            <form action="" wire:submit.prevent="updateLien(Object.fromEntries(new FormData($event.target)))">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="col-md-12 mt-2">
+                                            <div class="input-group" id="service_date" wire:ignore>
+                                                <x-flatpickr   id="service_date" class="flatpickr_date" name="lien_date" wire:model="lien_date_m"   value="{{$lien_date_m}}" />
+                                                <span class="input-group-text"><i class="mdi mdi-clock-outline"></i></span>
+                                            </div>
+                                            @error('service_date')
+                                            <div class="invalid-feedback show">
+                                                Please type a valid date.
+                                            </div>
+                                            @enderror
+                                        </div>
+                                        <div class="col-md-12 mt-2">
+                                            <textarea class="form-control  me-2" name="lien_info" wire:model="lien_info"  rows="5" placeholder="Enter note here..."></textarea>
+                                        </div>
+                                        <div class="col-md-12 mt-2">
+                                            <button type="button" class="btn btn-secondary btn-sm float-start m-1" wire:click="toggleFollowup"><i class="fas fa-chevron-left"></i> cancel</button>
+                                            <button type="submit" class="btn btn-success btn-sm float-start m-1" > <i class="fas fa-save"></i> save</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        @endif
                     </div>
                 </div>
             </div>
