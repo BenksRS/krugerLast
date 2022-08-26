8b968a2e6c53682f7ba46791c90179f61ca6e6d5
Felipe <felipepinheirosilva@gmail.com>
2022-08-26 10:37:59 -0400

    Revert "Update ShowMenu.php"
    This reverts commit 0dc52127132745f8aee117640682606ab9228c0a.

--- Modules/Menu/Http/Livewire/ShowMenu.php
+++ Modules/Menu/Http/Livewire/ShowMenu.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Menu\Http\Livewire;
 
-use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use Modules\Menu\Entities\MenuLink;
 
@@ -10,48 +9,11 @@ class ShowMenu extends Component {
 
     public $links;
 
-    public $user;
-
-    public function mount ()
-    {
-        $this->user = Auth::user()->load(['group']);
-
-        $this->getLinkGroup();
+    public function mount ( ) {
+        $this->links = MenuLink::query()->buildItems()->where('active', 'Y')->get();
     }
 
-    public function getLinkGroup ()
-    {
-
-        if ( $this->user->group ) {
-            $links = MenuLink::query();
-            $group = $links->whereHas('group', function ($query) {
-                return $query->where('group_id', $this->user->group->id)->where('visible', 'Y');
-            })->get();
-            $links = $group->map(function ($value) {
-                if ( !empty($value->group) ) {
-
-                    $group = $value->group;
-
-                    $value->order  = $group->order;
-                }
-
-                return $value;
-            });
-        }
-
-        $links = $links
-            ->map(function ($value) use ($links) {
-                $value->children = $links->where('link_id', $value->id);
-
-                return $value;
-
-            })->whereNull('link_id');
-
-        $this->links = $links;
-    }
-
-    public
-    function render ()
+    public function render ()
     {
         return view('menu::livewire.show-menu');
     }
