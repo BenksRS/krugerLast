0b9222e702dc7090cec492dc4c59c72b0978ec2d
Felipe <felipepinheirosilva@gmail.com>
2022-08-29 11:07:27 -0400

    sdsdsd

--- Modules/Assignments/Http/Livewire/Show/TabsPanel.php
+++ Modules/Assignments/Http/Livewire/Show/TabsPanel.php
@@ -14,17 +14,17 @@ class TabsPanel extends Component
     ];
 
     public $assignment;
-    public $isActive = 'comissions';
+    public $isActive = 'info-details';
 
     public $navs = [
-//        [
-//            'title' => 'Info Details',
-//            'status' => 'active',
-//            'href' => 'info-details',
-//            'key' => 'assignments_tab_info',
-//            'tab' => 'assignments::show.tabs.info',
-//            'category' => 'all',
-//        ],
+        [
+            'title' => 'Info Details',
+            'status' => 'active',
+            'href' => 'info-details',
+            'key' => 'assignments_tab_info',
+            'tab' => 'assignments::show.tabs.info',
+            'category' => 'all',
+        ],
         [
             'title' => 'Job Report',
             'status' => '',
