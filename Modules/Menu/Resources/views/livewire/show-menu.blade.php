e66dfd021c3a5cc69d3e344ea2f831431aeddc40
Michel Vieira <michelvieira@outlook.com>
2022-08-30 10:39:05 -0300

    Update show-menu.blade.php

--- Modules/Menu/Resources/views/livewire/show-menu.blade.php
+++ Modules/Menu/Resources/views/livewire/show-menu.blade.php
@@ -1,8 +1,8 @@
 <ul class="navbar-nav">
 	@foreach($links as $menu)
 		<li class="nav-item dropdown">
-			<?php $menu_link=$menu['url'];?>
-			<a class="nav-link dropdown-toggle arrow-none" href="{{ url("$menu_link") ?? '#' }}" id="topnav-dashboard" role="button">
+                <?php $menu_link = $menu['url'] != NULL ? url("{$menu['url']}") : '#'; ?>
+			<a class="nav-link dropdown-toggle arrow-none" href="{{ $menu_link }}" id="topnav-dashboard" role="button">
 				<i class="{{ $menu['icon'] ?? 'bx bx-home-circle' }} me-2"></i>
 				<span key="t-dashboards">{{ $menu['title'] }}</span>
 				@if(count($menu['children']) > 0)
@@ -12,7 +12,7 @@
 			@if(count($menu['children']) > 0)
 				<div class="dropdown-menu" aria-labelledby="topnav-dashboard">
 					@foreach($menu['children'] as $child)
-						<?php $child_link=$child['url'];?>
+                            <?php $child_link = $child['url']; ?>
 						<a href="{{url("$child_link") ?? '#' }}" class="dropdown-item" key="t-default">{{ $child['title'] }}</a>
 					@endforeach
 				</div>
