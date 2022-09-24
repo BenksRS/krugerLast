4930b3623cf4637374ce3af272b96b275fb6935d
Felipe <felipepinheirosilva@gmail.com>
2022-09-24 10:15:03 -0400

    ssdsd

--- Modules/Assignments/Entities/NoJob.php
+++ Modules/Assignments/Entities/NoJob.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Nojob extends Model
 {
     use HasFactory;
-
+    protected $table = 'nojob';
     protected $fillable = [
         ''
     ];
