fcd0f5c512aee50cffbffe549018b42153024201
Michel Vieira <michelvieira@outlook.com>
2022-11-02 10:02:11 -0300

    Update app.blade.php

--- Themes/system/views/components/layouts/app.blade.php
+++ Themes/system/views/components/layouts/app.blade.php
@@ -61,11 +61,9 @@
 							</button>
 							<div class="dropdown-menu dropdown-menu-end">
 								<!-- item-->
-								<a class="dropdown-item" href="#"><i class="bx bx-user font-size-16 align-middle me-1"></i>
+								<a class="dropdown-item" href="http://dashboard.callkruger.sys/profile"><i class="bx bx-user font-size-16 align-middle me-1"></i>
 									<span key="t-profile">Profile</span></a>
-								<a class="dropdown-item" href="#"><i class="bx bx-lock-open font-size-16 align-middle me-1"></i>
-									<span key="t-lock-screen">Lock screen</span></a>
-								<div class="dropdown-divider"></div>
+											<div class="dropdown-divider"></div>
 								<a class="dropdown-item text-danger" href="{{ route('auth.logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
 									<i class="bx bx-power-off font-size-16 align-middle me-1 text-danger"></i>
