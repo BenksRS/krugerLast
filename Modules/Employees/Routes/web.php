249557b7280bd069079efa3ad6303fbcaf52f3a3
Felipe <felipepinheirosilva@gmail.com>
2024-06-10 10:57:05 -0400

    dssdsd

--- Modules/Employees/Routes/web.php
+++ Modules/Employees/Routes/web.php
@@ -15,7 +15,7 @@ Route::prefix('/employees')->name('employees.')->group(function () {
     Route::get('/', [\Modules\Employees\Http\Controllers\EmployeesController::class, 'index'])->name('index');
     Route::get('/', [\Modules\Employees\Http\Controllers\EmployeesController::class, 'index'])->name('index');
     Route::get('/show/{id}', [\Modules\Employees\Http\Controllers\EmployeesController::class, 'show'])->name('show');
-    Route::get('/list_worker/{id}', [\Modules\Employees\Http\Controllers\EmployeesController::class, 'show'])->name('list_worker');
+    Route::get('/list_worker/{id}', [\Modules\Employees\Http\Controllers\EmployeesController::class, 'list_worker'])->name('list_worker');
     Route::get('/upload/', [\Modules\Employees\Http\Livewire\Show\Tabs\Receipts\Add::class, 'upload'])->name('upload');
     Route::get('/phpinfo', [\Modules\Employees\Http\Controllers\EmployeesController::class, 'phpinfo'])->name('phpinfo');
     Route::get('/rules', [\Modules\Employees\Http\Controllers\EmployeesController::class, 'rules'])->name('rules');
