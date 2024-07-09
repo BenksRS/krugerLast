85e0eaaba1d255c145986d3ef5cbe6c4a7d21687
Michel Vieira <michelvieira@outlook.com>
2024-07-09 19:07:13 -0300

    Update header.blade.php

--- Modules/Assignments/Resources/views/livewire/show/header.blade.php
+++ Modules/Assignments/Resources/views/livewire/show/header.blade.php
@@ -268,7 +268,7 @@
                                                     <li><button class="dropdown-item" wire:click="changeStatus(36)"  type="button">EXTREME</button></li>
                                                     <li><button class="dropdown-item" wire:click="changeStatus(37)"  type="button">NOBODY WAS HOME</button></li>
                                                     <li><button class="dropdown-item" wire:click="changeStatus(38)"  type="button">CRANE NEEDED</button></li>
-                                                    <li><button class="dropdown-item" wire:click="changeStatus(39)"  type="button">CRANE NEEDED</button></li>
+                                                    <li><button class="dropdown-item" wire:click="changeStatus(39)"  type="button">NOT ENOUGH TIME</button></li>
                                                 @endif
                                             </ul>
                                         </div>
