29c8279bc2dae92fafec725fa0c1b2ce3a84ba40
michelvieira <michelvieira@outlook.com>
2025-06-18 18:02:08 -0300

    Update Note.php

--- Modules/Notes/Entities/Note.php
+++ Modules/Notes/Entities/Note.php
@@ -34,6 +34,19 @@ class Note extends Model
         return date('m/d/Y g:i A', strtotime($this->created_at));
     }
 
+    public function getTextAttribute()
+    {
+        $escaped = e($this->text);
+
+        // 2. Transforma URLs em links clicáveis
+        $pattern = '/(https?:\/\/[^\s]+)/i';
+        $replacement = '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>';
+        $linkified = preg_replace($pattern, $replacement, $escaped);
+
+        // 3. Preserva quebras de linha (\n → <br>)
+        return nl2br($linkified);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class, 'created_by', 'id');
