b6a7f2af63e218ecc83e21336184bdeb5be8f362
michelvieira <michelvieira@outlook.com>
2025-06-19 04:49:32 -0300

    Update header.blade.php

--- Modules/Assignments/Resources/views/livewire/show/header.blade.php
+++ Modules/Assignments/Resources/views/livewire/show/header.blade.php
@@ -278,6 +278,7 @@
                                                     <li><button class="dropdown-item" wire:click="changeStatusScheduling(45)"  type="button">NADAL PROBLEM</button></li>
                                                     <li><button class="dropdown-item" wire:click="changeStatusScheduling(46)"  type="button">CRANE APPROVED</button></li>
                                                     <li><button class="dropdown-item" wire:click="changeStatusScheduling(49)"  type="button">CYNTHIA REVIEW</button></li>
+                                                    <li><button class="dropdown-item" wire:click="changeStatusScheduling(50)"  type="button">LANDFILL</button></li>
                                                 @endif
                                             </ul>
                                         </div>
