635680843c6025e01786c55f232888b098a56f27
michelvieira <michelvieira@outlook.com>
2025-11-28 02:53:33 -0300

    Update finance.blade.php

--- Modules/Reports/Resources/views/livewire/info/finance.blade.php
+++ Modules/Reports/Resources/views/livewire/info/finance.blade.php
@@ -5,11 +5,11 @@
             <div class="card-body">
                <div class="row">
                   @if($this->retorno)
-                     <p><b>Total Jobs:</b> {{$this->retorno['total']}} (Pending: {{$this->retorno['open']}} - Completed: {{$this->retorno['completed']}} - Closed: {{$this->retorno['closed']}})</p><br>
-                     <p><b>Total Jobs Direct Bill:</b> {{$this->retorno['direct_bill']['total']}} (Pending: {{$this->retorno['direct_bill']['open']}} - Completed: {{$this->retorno['direct_bill']['completed']}} - Closed: {{$this->retorno['direct_bill']['closed']}})</p><br>
-                     <p><b>Jobs Billed:</b> {{$this->retorno['billing']}} - Total Invoices amount: {{$this->retorno['total_billing']}} - Average amount: {{$this->retorno['media_billing']}}</p><br>
-                     <p><b>Jobs Billed Tree Removal:</b> {{$this->retorno['total_tree_billing']}}</p><br>
-                     <p><b>Jobs Paid:</b> {{$this->retorno['paid']}} - Total amount: {{$this->retorno['total_paid']}} - Average amount: {{$this->retorno['media_paid']}}</p><br>
+                     <span><b>Total Jobs:</b> {{$this->retorno['total']}} (Pending: {{$this->retorno['open']}} - Completed: {{$this->retorno['completed']}} - Closed: {{$this->retorno['closed']}})</span><br>
+                     <span><b>Total Jobs Direct Bill:</b> {{$this->retorno['direct_bill']['total']}} (Pending: {{$this->retorno['direct_bill']['open']}} - Completed: {{$this->retorno['direct_bill']['completed']}} - Closed: {{$this->retorno['direct_bill']['closed']}})</span><br>
+                     <span><b>Jobs Billed:</b> {{$this->retorno['billing']}} - Total Invoices amount: {{$this->retorno['total_billing']}} - Average amount: {{$this->retorno['media_billing']}}</span><br>
+                     <span><b>Jobs Billed Tree Removal:</b> {{$this->retorno['total_tree_billing']}}</span><br>
+                     <span><b>Jobs Paid:</b> {{$this->retorno['paid']}} - Total amount: {{$this->retorno['total_paid']}} - Average amount: {{$this->retorno['media_paid']}}</span><br>
 
                   @endif
                </div>
