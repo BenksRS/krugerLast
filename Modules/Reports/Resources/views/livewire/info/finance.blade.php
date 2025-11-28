26943c27645fc0132bf5f4e7fe439aa21afdd091
michelvieira <michelvieira@outlook.com>
2025-11-28 02:52:15 -0300

    Update finance.blade.php

--- Modules/Reports/Resources/views/livewire/info/finance.blade.php
+++ Modules/Reports/Resources/views/livewire/info/finance.blade.php
@@ -6,10 +6,10 @@
                <div class="row">
                   @if($this->retorno)
                      <p><b>Total Jobs:</b> {{$this->retorno['total']}} (Pending: {{$this->retorno['open']}} - Completed: {{$this->retorno['completed']}} - Closed: {{$this->retorno['closed']}})</p><br>
-                     <b>Total Jobs Direct Bill:</b> {{$this->retorno['direct_bill']['total']}} (Pending: {{$this->retorno['direct_bill']['open']}} - Completed: {{$this->retorno['direct_bill']['completed']}} - Closed: {{$this->retorno['direct_bill']['closed']}})<br>
-                     <b>Jobs Billed:</b> {{$this->retorno['billing']}} - Total Invoices amount: {{$this->retorno['total_billing']}} - Average amount: {{$this->retorno['media_billing']}}<br>
-                     <b>Jobs Billed Tree Removal:</b> {{$this->retorno['total_tree_billing']}}<br>
-                     <b>Jobs Paid:</b> {{$this->retorno['paid']}} - Total amount: {{$this->retorno['total_paid']}} - Average amount: {{$this->retorno['media_paid']}}<br>
+                     <p><b>Total Jobs Direct Bill:</b> {{$this->retorno['direct_bill']['total']}} (Pending: {{$this->retorno['direct_bill']['open']}} - Completed: {{$this->retorno['direct_bill']['completed']}} - Closed: {{$this->retorno['direct_bill']['closed']}})</p><br>
+                     <p><b>Jobs Billed:</b> {{$this->retorno['billing']}} - Total Invoices amount: {{$this->retorno['total_billing']}} - Average amount: {{$this->retorno['media_billing']}}</p><br>
+                     <p><b>Jobs Billed Tree Removal:</b> {{$this->retorno['total_tree_billing']}}</p><br>
+                     <p><b>Jobs Paid:</b> {{$this->retorno['paid']}} - Total amount: {{$this->retorno['total_paid']}} - Average amount: {{$this->retorno['media_paid']}}</p><br>
 
                   @endif
                </div>
