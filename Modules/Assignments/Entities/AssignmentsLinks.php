e67d79a6309988bc4cd15f73fcbcce37c451105b
michelvieira <michelvieira@outlook.com>
2025-12-17 17:27:12 -0300

    Update AssignmentsLinks.php

--- Modules/Assignments/Entities/AssignmentsLinks.php
+++ Modules/Assignments/Entities/AssignmentsLinks.php
@@ -5,16 +5,19 @@ namespace Modules\Assignments\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class AssignmentsLinks extends Model
-{
+class AssignmentsLinks extends Model {
+
     use HasFactory;
-    protected $table = 'assignments_links';
+
+    protected $table    = 'assignments_links';
+
     protected $fillable = [
         'assignment_id',
         'name',
         'link'
     ];
-    protected $appends = ['link_view'];
+
+    protected $appends  = ['link_view'];
 
     public function assignment()
     {
@@ -26,29 +29,34 @@ class AssignmentsLinks extends Model
         $rawLink = trim($this->link);
 
         if (!$rawLink) {
-            return null;
+            return NULL;
         }
 
         // 1. Garante que o hyperlink tenha protocolo
         $hasProtocol = str_starts_with($rawLink, 'http://') || str_starts_with($rawLink, 'https://');
-        $hyperlink = $hasProtocol ? $rawLink : 'https://' . $rawLink;
+        $hyperlink   = $hasProtocol ? $rawLink : 'https://'.$rawLink;
 
-        // 2. Remove protocolo para trabalhar o label
+        // 2. Remove protocolo
         $label = preg_replace('#^https?://#i', '', $rawLink);
 
         // 3. Remove www.
         $label = preg_replace('#^www\.#i', '', $label);
 
-        // 4. Remove query string (? e tudo depois)
+        // 4. Remove query string
         $label = explode('?', $label)[0];
 
+        // 5. Remove tudo após a primeira "/"
+        $label = explode('/', $label)[0];
+
         return [
-            'label'     => $label,
+            'label'     => 'www.'.$label,
             'hyperlink' => $hyperlink,
         ];
     }
+
     protected static function newFactory()
     {
         return \Modules\Assignments\Database\factories\AssignmentsPhonesFactory::new();
     }
+
 }
