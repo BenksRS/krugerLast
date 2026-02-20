a1e21b6d0df91fbe95ee3c40a20698c6e135cd8d
michelvieira <michelvieira@outlook.com>
2026-02-20 18:54:10 -0300

    Update Index.php

--- Modules/Car/Http/Livewire/List/Index.php
+++ Modules/Car/Http/Livewire/List/Index.php
@@ -9,50 +9,50 @@ use Modules\Car\Entities\Car;
 use Modules\Car\Entities\CarsLogs;
 use Modules\Car\Repositories\CarRepository;
 
-class Index extends Component
-{
+class Index extends Component {
+
     public $auto;
+
     public $driver;
 
-    public $show=false;
+    public $show = FALSE;
 
     use WithPagination;
+
     protected $paginationTheme = 'bootstrap';
 
-    public $searchAssignment;
+    public    $searchAssignment;
 
+    public $columns         = ['Auto', 'Driver', 'E-pass', 'Plate', 'Tag Expires', 'Insurance Expires', 'VIN'];
 
-
-    public $columns = ['Auto','Driver','E-pass','Plate','Tag Expires','Insurance Expires','VIN'];
     public $selectedColumns = [];
-    public $selectedRows = 100;
+
+    public $selectedRows    = 100;
 
     public $sortBy;
 
     public function mount()
     {
         $this->selectedColumns = $this->columns;
-        $this->auto = 'teste';
+        $this->auto            = 'teste';
 
-        $this->sortBy  = 'auto';
-
+        $this->sortBy = 'auto';
     }
+
     public function updatingSearchAssignment()
     {
         $this->resetPage();
     }
 
-
-
-
-
     public function render()
     {
         $searchAssignment = $this->searchAssignment;
-        $list = CarRepository::Searchtop($searchAssignment)->get();
+        $list             = CarRepository::Searchtop($searchAssignment)->get();
 
-        if($this->sortBy) {
+        if ($this->sortBy != 'auto') {
             $list = $list->sortByDesc($this->sortBy);
+        } else {
+            $list = $list->sortBy($this->sortBy);
         }
 
         $items = $list->forPage($this->page, $this->selectedRows);
@@ -60,7 +60,8 @@ class Index extends Component
         $list = new LengthAwarePaginator($items, $list->count(), $this->selectedRows, $this->page);
 
         return view('car::livewire.list.index', [
-            'list' =>$list
+            'list' => $list
         ]);
     }
+
 }
