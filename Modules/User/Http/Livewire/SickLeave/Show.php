d31559b3c4856d839c094653df8bbbf3bf514211
michelvieira <michelvieira@outlook.com>
2026-07-21 12:13:08 -0300

    Update

--- Modules/User/Http/Livewire/SickLeave/Show.php
+++ Modules/User/Http/Livewire/SickLeave/Show.php
@@ -7,11 +7,18 @@ use Modules\User\Entities\User;
 
 class Show extends Component {
 
+    public    $search;
+
     protected $listeners = ['$refresh'];
 
     public function render()
     {
         $employees = User::where('active', 'Y')
+            ->where(function($query) {
+                $query->when($this->search, function($query, $search) {
+                    $query->where('name', 'like', '%'.$search.'%');
+                });
+            })
             ->withSum(['sickLeaves as sick_days_used' => function ($query) {
                 $query->where('year', now()->year)->where('status', 'Y');
             }], 'days')
--- Modules/User/Resources/views/livewire/sick-leave/show.blade.php
+++ Modules/User/Resources/views/livewire/sick-leave/show.blade.php
@@ -8,7 +8,19 @@
                <div class="row">
                   <div class="col-12">
 
-                     <div class="table-responsive mt-3">
+                     <form class="d-block">
+                        <div class="position-relative col-lg-12">
+                           <input type="text" class="form-control" placeholder="Search here.." wire:model.debounce.500ms="search" type="search">
+                        </div>
+                     </form>
+
+                     <div wire:loading.flex wire:target="search" class="justify-content-center mt-3">
+                        <div class="spinner-border" role="status">
+                           <span class="visually-hidden">Loading...</span>
+                        </div>
+                     </div>
+
+                     <div class="table-responsive mt-3" wire:loading.remove wire:target="search">
                         <table class="table table-hover table-bordered listtable mb-0 align-middle">
                            <thead>
                               <tr>
@@ -23,10 +35,10 @@
                                  <tr wire:key="employee-{{ $employee->id }}">
                                     <td class="text-center" style="width: 100px">{{ $employee->id }}</td>
                                     <td>{{ $employee->name }}</td>
-                                    <td class="text-center">
+                                    <td  style="width: 150px" class="text-center">
                                        {{ $employee->sick_days_used ?? 0 }}/3
                                     </td>
-                                    <td style="width: 250px">
+                                    <td style="width: 275px">
                                        <div class="d-flex gap-2">
                                           <button class="btn btn-info btn-label waves-light" type="button"
                                                   wire:click="$emit('sickLeaveDetails', {{ $employee->id }}, '{{ $employee->name }}')">
