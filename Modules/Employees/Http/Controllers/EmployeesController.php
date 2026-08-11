b9022b1a0850c48d2e23ecee71a90c7c8b5f77a7
Felipe <felipepinheirosilva@gmail.com>
2026-08-11 11:04:50 -0400

    DWWD

--- Modules/Employees/Http/Controllers/EmployeesController.php
+++ Modules/Employees/Http/Controllers/EmployeesController.php
@@ -697,7 +697,7 @@ $total_commission=0;
         $jobs_list=array(11,26);
         $assignment = AssignmentFinanceRepository::find($id);
         $workers = JobReportWorkers::where('assignment_id', $id)->whereIn('job_type_id',$jobs_list)->pluck('worker_id')->toArray();
-        dump($workers);
+//        dump($workers);
         $rulles = EmployeeRules::whereIn('user_id', $workers)
             ->where('type', 'L')
             ->get();
