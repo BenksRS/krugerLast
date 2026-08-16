10196a477274d818739e06294d303d69f8664434
michelvieira <michelvieira@outlook.com>
2026-08-16 09:45:57 -0300

    Update roof-tarp.blade.php

--- Modules/Assignments/Resources/views/livewire/show/tabs/job-report/roof-tarp.blade.php
+++ Modules/Assignments/Resources/views/livewire/show/tabs/job-report/roof-tarp.blade.php
@@ -301,39 +301,41 @@
                 <div class="row">
                     <div class="col-xl-6 col-sm-6">
                         <label class="form-label">Workers</label>
-                        <div class="row" wire:ignore>
+                        <div wire:ignore>
                             @php
                                 $grouped = $workers->groupBy(function($wk) {
-                                    return $wk->user->team ?? 'no_team';
+                                    return $wk->team ?? 'no_team';
                                 })->sortKeys();
                             @endphp
 
                             @foreach($grouped as $teamKey => $teamWorkers)
-                                <div class="col-12 mb-2">
-                                    <h5 class="mb-2">
-                                        {{ $teamKey === 'no_team' ? 'Sem Time' : 'Time '.$teamKey }}
-                                    </h5>
+                                <div class="row">
+                                    <div class="col-12 mb-2">
+                                        <h5 class="mb-2">
+                                            {{ $teamKey === 'no_team' ? 'Sem Time' : 'Time '.$teamKey }}
+                                        </h5>
+                                    </div>
                                 </div>
 
                                 @foreach($teamWorkers as $wk)
-                                    <div class="col-auto float-start">
-                                        <div class="form-check form-checkbox-outline form-check-primary mb-3">
-                                            <input
-                                              class="form-check-input"
-                                              type="checkbox"
-                                              id="checkWorker{{ $wk->user->id }}"
-                                              wire:click="syncWorkers({{ $wk->user->id }})"
-                                              value="{{ $wk->user->id }}"
-                                              {{ ($workersDB && $workersDB->contains($wk->user->id)) ? 'checked' : '' }}
-                                            >
-                                            <label class="form-check-label" for="checkWorker{{ $wk->user->id }}">
-                                                {{ $wk->user->name }}
-                                            </label>
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="form-check form-checkbox-outline form-check-primary mb-3">
+                                                <input
+                                                  class="form-check-input"
+                                                  type="checkbox"
+                                                  id="checkWorker{{ $wk->user->id }}"
+                                                  wire:click="syncWorkers({{ $wk->user->id }})"
+                                                  value="{{ $wk->user->id }}"
+                                                  {{ ($workersDB && $workersDB->contains($wk->user->id)) ? 'checked' : '' }}
+                                                >
+                                                <label class="form-check-label" for="checkWorker{{ $wk->user->id }}">
+                                                    {{ $wk->user->name }}
+                                                </label>
+                                            </div>
                                         </div>
                                     </div>
                                 @endforeach
-
-                                <div class="col-12 mb-3"></div>
                             @endforeach
                         </div>
 
