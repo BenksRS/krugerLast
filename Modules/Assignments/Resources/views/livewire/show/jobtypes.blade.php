4cc2ba3dc0a37f564ec26a4ce91e970f95521114
michelvieira <michelvieira@outlook.com>
2026-08-26 18:03:50 -0300

    Update jobtypes.blade.php

--- Modules/Assignments/Resources/views/livewire/show/jobtypes.blade.php
+++ Modules/Assignments/Resources/views/livewire/show/jobtypes.blade.php
@@ -6,34 +6,29 @@
             @if(in_array($js->id, [18]) && !in_array(Auth::user()->id, [1, 2, 10, 25]))
               @continue
             @endif
-            @switch($js->id)
+               @if($jbSelectedSingle->isNotEmpty())
+                  <div class="col-lg-5">
+                     <div class="form-check form-check-left mb-3">
+                        <label class="form-check-label {{ ($js->type == 'S') ? (($jbSelected->contains($js->id)) ? '' : 'label_disable') :''  }}"   for="{{$js->name}}{{$js->id}}">
+                           <input class="form-check-input check_types" type="checkbox" data-type="{{$js->type}}" wire:click="update({{$js->id}})"
+                             {{ ($jbSelected->contains($js->id)) ? 'checked' :''    }}  {{ ($js->type == 'S') ? (($jbSelected->contains($js->id)) ? '' : ' disabled="disabled"') :''  }} >
 
-                @default
-                    @if($jbSelectedSingle->isNotEmpty())
-                        <div class="col-lg-5">
-                            <div class="form-check form-check-left mb-3">
-                                <label class="form-check-label {{ ($js->type == 'S') ? (($jbSelected->contains($js->id)) ? '' : 'label_disable') :''  }}"   for="{{$js->name}}{{$js->id}}">
-                                    <input class="form-check-input check_types" type="checkbox" data-type="{{$js->type}}" wire:click="update({{$js->id}})"
-                                            {{ ($jbSelected->contains($js->id)) ? 'checked' :''    }}  {{ ($js->type == 'S') ? (($jbSelected->contains($js->id)) ? '' : ' disabled="disabled"') :''  }} >
+                           {{$js->name}}
+                        </label>
+                     </div>
+                  </div>
+               @else
+                  <div class="col-lg-5">
+                     <div class="form-check form-check-left mb-3">
+                        <label class="form-check-label" for="{{$js->name}}{{$js->id}}">
+                           <input class="form-check-input check_types" type="checkbox" data-type="{{$js->type}}" wire:click="update({{$js->id}})"
+                             {{ ($jbSelected->contains($js->id)) ? 'checked' :'' }}  >
 
-                                    {{$js->name}}
-                                </label>
-                            </div>
-                        </div>
-                    @else
-                        <div class="col-lg-5">
-                            <div class="form-check form-check-left mb-3">
-                                <label class="form-check-label" for="{{$js->name}}{{$js->id}}">
-                                    <input class="form-check-input check_types" type="checkbox" data-type="{{$js->type}}" wire:click="update({{$js->id}})"
-                                            {{ ($jbSelected->contains($js->id)) ? 'checked' :'' }}  >
-
-                                    {{$js->name}}
-                                </label>
-                            </div>
-                        </div>
-                    @endif
-                @break
-            @endswitch
+                           {{$js->name}}
+                        </label>
+                     </div>
+                  </div>
+               @endif
 
 
 
