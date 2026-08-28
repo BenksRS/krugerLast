99ed6ebd77d7bead444a465cdcc862eb86589152
michelvieira <michelvieira@outlook.com>
2026-08-27 22:41:16 -0300

    Update Tab Tree Estimate

--- Modules/Assignments/Http/Livewire/Show/Tabs/CraneNeeded.php
+++ Modules/Assignments/Http/Livewire/Show/Tabs/CraneNeeded.php
@@ -11,39 +11,35 @@ class CraneNeeded extends Component
     public $assignment;
     public $user;
 
-    public $crane_needed;
-    public $crane_nadal;
-    public $crane_request;
-    public $crane_notes;
-
     protected $rules = [
-        'crane_needed' => 'required|in:Y,N',
-        'crane_nadal' => 'nullable',
-        'crane_request' => 'nullable',
-        'crane_notes' => 'nullable',
+        'assignment.crane_needed' => 'required|in:Y,N',
+        'assignment.crane_nadal' => 'nullable',
+        'assignment.crane_request' => 'nullable',
+        'assignment.crane_notes' => 'nullable',
     ];
 
     public function mount(Assignment $assignment)
     {
         $this->assignment = $assignment;
         $this->user = Auth::user();
+    }
 
-        $this->crane_needed = $this->assignment->crane_needed ?? 'N';
-        $this->crane_nadal = $this->assignment->crane_nadal;
-        $this->crane_request = $this->assignment->crane_request;
-        $this->crane_notes = $this->assignment->crane_notes;
+    public function getHasChangesProperty()
+    {
+        return $this->assignment->isDirty([
+            'crane_needed',
+            'crane_nadal',
+            'crane_request',
+            'crane_notes',
+        ]);
     }
 
     public function save()
     {
         $this->validate();
 
-        $this->assignment->update([
-            'crane_needed' => $this->crane_needed,
-            'crane_nadal' => $this->crane_nadal,
-            'crane_request' => $this->crane_request,
-            'crane_notes' => $this->crane_notes,
-        ]);
+        $this->assignment->save();
+
         $this->emit('craneNeededSaved');
     }
 
--- Modules/Assignments/Resources/views/livewire/show/tabs/crane-needed.blade.php
+++ Modules/Assignments/Resources/views/livewire/show/tabs/crane-needed.blade.php
@@ -8,29 +8,29 @@
                <label class="col-md-1 col-form-label">Tree Estimate</label>
                <div class="col-md-6 align-items-center d-flex">
                   <div class="form-check form-check-inline">
-                     <input type="radio" wire:model="crane_needed" value="Y" class="form-check-input" id="crane_needed_y"> <label class="form-check-label" for="crane_needed_y">Yes</label>
+                     <input type="radio" wire:model="assignment.crane_needed" value="Y" class="form-check-input" id="crane_needed_y"> <label class="form-check-label" for="crane_needed_y">Yes</label>
                   </div>
                   <div class="form-check form-check-inline">
-                     <input type="radio" wire:model="crane_needed" value="N" class="form-check-input" id="crane_needed_n"> <label class="form-check-label" for="crane_needed_n">No</label>
+                     <input type="radio" wire:model="assignment.crane_needed" value="N" class="form-check-input" id="crane_needed_n"> <label class="form-check-label" for="crane_needed_n">No</label>
                   </div>
-                  @error('crane_needed') <span class="text-danger d-block">{{ $message }}</span> @enderror
+                  @error('assignment.crane_needed') <span class="text-danger d-block">{{ $message }}</span> @enderror
                </div>
             </div>
 
-            @if($crane_needed === 'Y')
+            @if($assignment->crane_needed === 'Y')
                <div class="row mb-3">
                   <label class="col-md-1 col-form-label">Crane Request</label>
                   <div class="col-md-4">
-                     <input type="text" wire:model.defer="crane_request" class="form-control" placeholder="Crane Request...">
-                     @error('crane_request') <span class="text-danger">{{ $message }}</span> @enderror
+                     <input type="text" wire:model.debounce.500ms="assignment.crane_request" class="form-control" placeholder="Crane Request...">
+                     @error('assignment.crane_request') <span class="text-danger">{{ $message }}</span> @enderror
                   </div>
                </div>
 
                <div class="row mb-3">
                   <label class="col-md-1 col-form-label">Crane Nadal</label>
                   <div class="col-md-4">
-                     <input type="text" wire:model.defer="crane_nadal" class="form-control" placeholder="Crane Nadal...">
-                     @error('crane_nadal') <span class="text-danger">{{ $message }}</span> @enderror
+                     <input type="text" wire:model.debounce.500ms="assignment.crane_nadal" class="form-control" placeholder="Crane Nadal...">
+                     @error('assignment.crane_nadal') <span class="text-danger">{{ $message }}</span> @enderror
                   </div>
                </div>
             @endif
@@ -38,18 +38,19 @@
             <div class="row mb-3">
                <label class="col-md-1 col-form-label">Crane Notes</label>
                <div class="col-md-6">
-                  <textarea wire:model.defer="crane_notes" class="form-control" rows="15" placeholder="Crane Notes..."></textarea>
-                  @error('crane_notes') <span class="text-danger">{{ $message }}</span> @enderror
+                  <textarea wire:model.debounce.500ms="assignment.crane_notes" class="form-control" rows="15" placeholder="Crane Notes..."></textarea>
+                  @error('assignment.crane_notes') <span class="text-danger">{{ $message }}</span> @enderror
                </div>
             </div>
 
-            @if($crane_notes == "")
-            <div class="row">
-               <div class="col-md-7">
-                  <button type="submit" class="btn btn-success float-end">Save</button>
+            @if($this->hasChanges)
+               <div class="row">
+                  <div class="col-md-7">
+                     <button type="submit" class="btn btn-success float-end">Save</button>
+                  </div>
                </div>
-            </div>
             @endif
+
          </div>
       </div>
    </form>
