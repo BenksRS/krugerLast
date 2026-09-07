342e2412cde59162677a5073e025c56500dd4f8b
Felipe <felipepinheirosilva@gmail.com>
2026-09-07 12:40:13 -0400

    saassa

--- Modules/Assignments/Http/Controllers/AssignmentsController.php
+++ Modules/Assignments/Http/Controllers/AssignmentsController.php
@@ -138,14 +138,14 @@ class AssignmentsController extends Controller {
         }
         $gallery_imags=collect((object)$gallery_imags)->toArray();
 
-        dump($gallery_imags);
+//        dump($gallery_imags);
 
 
         $job_info = (object)[
             'job_id' => $assignment->id,
             'first_name' => $assignment->first_name,
             'last_name' => $assignment->last_name,
-            'pictures' =>(object)[$gallery_imags]
+            'pictures' =>[$gallery_imags]
         ];
 
 
