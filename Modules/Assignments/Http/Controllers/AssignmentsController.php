cb44a9f9f4b30e59e88d9e00c0649e4d203115c7
Felipe <felipepinheirosilva@gmail.com>
2026-09-07 12:37:22 -0400

    sddsds

--- Modules/Assignments/Http/Controllers/AssignmentsController.php
+++ Modules/Assignments/Http/Controllers/AssignmentsController.php
@@ -141,7 +141,7 @@ class AssignmentsController extends Controller {
             'job_id' => $assignment->id,
             'first_name' => $assignment->first_name,
             'last_name' => $assignment->last_name,
-            'pictures' =>(object)[$gallery_imags->items]
+            'pictures' =>(object)[$gallery_imags['items']]
         ];
 
 
